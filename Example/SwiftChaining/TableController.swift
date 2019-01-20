809ddaeddfccf23d1405d9ef0f43e1310987d669
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-20 21:37:08 +0900

    TableSection

--- Example/SwiftChaining/TableController.swift
+++ Example/SwiftChaining/TableController.swift
@@ -5,47 +5,6 @@
 import Foundation
 import Chaining
 
-final class TableSection {
-    enum Event {
-        case all([AnyCellData], String?)
-        case rows(ArrayHolder<AnyCellData>.Event)
-        case title(String?)
-    }
-    
-    let core = SenderCore<TableSection>()
-    
-    let title: Holder<String?>
-    let rows: ArrayHolder<AnyCellData>
-    
-    var pool = ObserverPool()
-    
-    convenience init() {
-        self.init(title: nil, rows: [])
-    }
-    
-    init(title: String?, rows: [AnyCellData]) {
-        self.title = Holder(title)
-        self.rows = ArrayHolder(rows)
-        
-        self.pool += self.rows.chain().to { .rows($0) }.receive(self).end()
-        self.pool += self.title.chain().to { .title($0) }.receive(self).end()
-    }
-}
-
-extension TableSection: Fetchable {
-    typealias SendValue = Event
-    
-    func fetchedValue() -> TableSection.SendValue? {
-        return .all(self.rows.rawArray, self.title.value)
-    }
-}
-
-extension TableSection: Receivable {
-    func receive(value: TableSection.Event) {
-        self.broadcast(value: value)
-    }
-}
-
 class TableController {
     typealias SectionArray = RelayableArrayHolder<TableSection>
     
@@ -54,7 +13,7 @@ class TableController {
     init() {
         let section0 = TableSection(title: "Section 0", rows:[CustomCellData(number: 10)])
         let section1 = TableSection(title: "Section 1", rows:[])
-        self.sections = RelayableArrayHolder([section0, section1])
+        self.sections = SectionArray([section0, section1])
     }
     
     func addRow() {
