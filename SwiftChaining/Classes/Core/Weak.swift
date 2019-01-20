d2a7a23c05972c2dfa51b5dd86fa4693bbec7748
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-20 15:00:55 +0900

    internal Weak

--- SwiftChaining/Classes/Core/Weak.swift
+++ SwiftChaining/Classes/Core/Weak.swift
@@ -4,22 +4,22 @@
 
 import Foundation
 
-public class Weak<T: AnyObject> {
+internal class Weak<T: AnyObject> {
     internal private(set) weak var value: T?
     internal let id: ObjectIdentifier
 
-    init(_ value: T) {
+    internal init(_ value: T) {
         self.value = value
         self.id = ObjectIdentifier(value)
     }
 }
 
 extension Weak: Hashable {
-    public var hashValue: Int {
+    internal var hashValue: Int {
         return id.hashValue
     }
     
-    public static func == (lhs: Weak<T>, rhs: Weak<T>) -> Bool {
+    internal static func == (lhs: Weak<T>, rhs: Weak<T>) -> Bool {
         return lhs.id == rhs.id
     }
 }
