acadb782aad28d52c97a690e393e1ca0ddd3f376
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-20 23:19:00 +0900

    mac project

---
+++ SwiftChaining_mac/Chaining/Chaining.h
@@ -0,0 +1,11 @@
+//
+//  Chaining.h
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Chaining.
+FOUNDATION_EXPORT double ChainingVersionNumber;
+
+//! Project version string for Chaining.
+FOUNDATION_EXPORT const unsigned char ChainingVersionString[];
