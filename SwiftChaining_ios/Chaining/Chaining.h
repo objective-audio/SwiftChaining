059b82abb8e2214b58808595568a7a091da815fd
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-20 23:38:48 +0900

    ios project

---
+++ SwiftChaining_ios/Chaining/Chaining.h
@@ -0,0 +1,18 @@
+//
+//  Chaining.h
+//  Chaining
+//
+//  Created by 八十嶋祐樹 on 2019/01/20.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chaining.
+FOUNDATION_EXPORT double ChainingVersionNumber;
+
+//! Project version string for Chaining.
+FOUNDATION_EXPORT const unsigned char ChainingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chaining/PublicHeader.h>
+
+
