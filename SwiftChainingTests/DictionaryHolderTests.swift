d7b1908ecfe2eb11b38e859e2fb38364bc8f2a46
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-26 15:14:45 +0900

    separate tests

--- SwiftChainingTests/DictionaryHolderTests.swift
+++ SwiftChainingTests/DictionaryHolderTests.swift
@@ -20,7 +20,7 @@ class DictionaryHolderTests: XCTestCase {
         XCTAssertEqual(dictionary.rawDictionary, [:])
     }
     
-    func testSetDictionaryWithRelayableValue() {
+    func testSetDictionary() {
         let dictionary = DictionaryHolder([10: "10", 20: "20"])
         
         XCTAssertEqual(dictionary.rawDictionary, [10: "10", 20: "20"])
@@ -29,26 +29,8 @@ class DictionaryHolderTests: XCTestCase {
         
         XCTAssertEqual(dictionary.rawDictionary, [30: "30", 40: "40", 50: "50"])
     }
-    
-    func testSetDictionaryWithSendableValue() {
-        let holder1 = Holder(1)
-        let holder2 = Holder(2)
-        let holder3 = Holder(3)
-        
-        let dictionary = DictionaryHolder([1: holder1, 2: holder2])
-        
-        XCTAssertEqual(dictionary.rawDictionary.count, 2)
-        XCTAssertEqual(dictionary[1], Holder(1))
-        XCTAssertEqual(dictionary[2], Holder(2))
-        
-        dictionary.set([3: holder3])
-        
-        XCTAssertEqual(dictionary.count, 1)
-        XCTAssertEqual(dictionary[3], Holder(3))
 
-    }
-
-    func testReplaceValueWithRelayableValue() {
+    func testReplaceValue() {
         let dictionary = DictionaryHolder([10: "10", 20: "20", 30: "30"])
 
         XCTAssertEqual(dictionary.rawDictionary, [10: "10", 20: "20", 30: "30"])
@@ -60,27 +42,6 @@ class DictionaryHolderTests: XCTestCase {
         dictionary[30] = "300"
         
         XCTAssertEqual(dictionary.rawDictionary, [10: "10", 20: "200", 30: "300"])
-    }
-
-    func testReplaceValueWithSendableValue() {
-        let holder1 = Holder(1)
-        let holder2 = Holder(2)
-        let holder3 = Holder(3)
-        let holder2b = Holder(22)
-        let holder3b = Holder(33)
-
-        let dictionary = DictionaryHolder([1: holder1, 2: holder2, 3: holder3])
-
-        XCTAssertEqual(dictionary.rawDictionary, [1: holder1, 2: holder2, 3: holder3])
-
-        dictionary.replace(key: 2, value: holder2b)
-
-        XCTAssertEqual(dictionary.count, 3)
-        XCTAssertEqual(dictionary.rawDictionary, [1: holder1, 2: holder2b, 3: holder3])
-        
-        dictionary[3] = holder3b
-        
-        XCTAssertEqual(dictionary.rawDictionary, [1: holder1, 2: holder2b, 3: holder3b])
     }
 
     func testRemoveValue() {
@@ -125,21 +86,14 @@ class DictionaryHolderTests: XCTestCase {
         XCTAssertEqual(dictionary.count, 3)
     }
     
-    func testSubscriptGetWithRelayableValue() {
+    func testSubscriptGet() {
         let dictionary = DictionaryHolder([10: "10"])
         
         XCTAssertEqual(dictionary[10], "10")
         XCTAssertNil(dictionary[20])
     }
     
-    func testSubscriptGetWithSendableValue() {
-        let dictionary = DictionaryHolder([10: Holder(10)])
-        
-        XCTAssertEqual(dictionary[10], Holder(10))
-        XCTAssertNil(dictionary[20])
-    }
-    
-    func testSubscriptSetWithRelayableValue() {
+    func testSubscriptSet() {
         let dictionary = DictionaryHolder([10: "10"])
         
         dictionary[20] = "20"
@@ -155,23 +109,7 @@ class DictionaryHolderTests: XCTestCase {
         XCTAssertEqual(dictionary.rawDictionary, [10: "100"])
     }
     
-    func testSubscriptSetWithSendableValue() {
-        let dictionary = DictionaryHolder([10: Holder(10)])
-        
-        dictionary[20] = Holder(20)
-        
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(10), 20: Holder(20)])
-        
-        dictionary[10] = Holder(100)
-        
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(100), 20: Holder(20)])
-        
-        dictionary[20] = nil
-        
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(100)])
-    }
-    
-    func testEventWithRelayableValues() {
+    func testEvent() {
         let dictionary = DictionaryHolder([10: "10", 20: "20"])
 
         var received: [DictionaryHolder<Int, String>.Event] = []
@@ -244,97 +182,6 @@ class DictionaryHolderTests: XCTestCase {
         }
 
         XCTAssertEqual(dictionary.rawDictionary, [:])
-        
-        observer.invalidate()
-    }
-
-    func testEventWithSendableElements() {
-        let dictionary = RelayableDictionaryHolder([10: Holder(10), 20: Holder(20)])
-
-        var received: [RelayableDictionaryHolder<Int, Holder<Int>>.Event] = []
-
-        let observer = dictionary.chain().do { received.append($0) }.sync()
-
-        XCTAssertEqual(received.count, 1)
-
-        if case .fetched(let elements) = received[0] {
-            XCTAssertEqual(elements, [10: Holder<Int>(10), 20: Holder<Int>(20)])
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        dictionary.insert(key: 100, value: Holder(100))
-
-        XCTAssertEqual(received.count, 2)
-
-        if case .inserted(let key, let value) = received[1] {
-            XCTAssertEqual(value, Holder(100))
-            XCTAssertEqual(key, 100)
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(10), 20: Holder(20), 100: Holder(100)])
-
-        dictionary.removeValue(forKey: 20)
-
-        XCTAssertEqual(received.count, 3)
-
-        if case .removed(let key, let value) = received[2] {
-            XCTAssertEqual(value, Holder(20))
-            XCTAssertEqual(key, 20)
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(10), 100: Holder(100)])
-
-        dictionary[10]?.value = 11
-
-        XCTAssertEqual(received.count, 4)
-
-        if case .relayed(let relayedValue, let key, let value) = received[3] {
-            XCTAssertEqual(value, Holder(11))
-            XCTAssertEqual(key, 10)
-            XCTAssertEqual(relayedValue, 11)
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(11), 100: Holder(100)])
-
-        dictionary.replace(key: 100, value: Holder(500))
-
-        XCTAssertEqual(received.count, 5)
-
-        if case .replaced(let key, let value) = received[4] {
-            XCTAssertEqual(value, Holder(500))
-            XCTAssertEqual(key, 100)
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        XCTAssertEqual(dictionary.rawDictionary, [10: Holder(11), 100: Holder(500)])
-
-        dictionary.set([1000: Holder(1000), 999: Holder(999)])
-
-        XCTAssertEqual(received.count, 6)
-
-        if case .any(let dictionary) = received[5] {
-            XCTAssertEqual(dictionary, [1000: Holder(1000), 999: Holder(999)])
-        } else {
-            XCTAssertTrue(false)
-        }
-
-        dictionary.removeAll()
-
-        XCTAssertEqual(received.count, 7)
-
-        if case .any(let dictionary) = received[6] {
-            XCTAssertEqual(dictionary, [:])
-        } else {
-            XCTAssertTrue(false)
-        }
         
         observer.invalidate()
     }
