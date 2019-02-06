c450e8374b81e1ea0e47edd4e7b40b9e48dd63cd
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-06 23:38:49 +0900

    Separate Receive

--- SwiftChaining/Core/Do.swift
+++ SwiftChaining/Core/Do.swift
@@ -20,22 +20,4 @@ extension Chain {
             return result
         }
     }
-    
-    public func receive<R: Receivable>(_ receiver: R) -> Chain<HandlerOut, HandlerIn, Sender> where R.ReceiveValue == HandlerOut {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value)
-        }
-    }
-    
-    public func receive<R: Receivable>(_ receiver: R) -> Chain<HandlerOut, HandlerIn, Sender> where R.ReceiveValue == HandlerOut? {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value)
-        }
-    }
-    
-    public func receive<R: Receivable>(_ receiver: R) -> Chain<HandlerOut, HandlerIn, Sender> where R.ReceiveValue == Void {
-        return self.do { [weak receiver] _ in
-            receiver?.receive(value: ())
-        }
-    }
 }
