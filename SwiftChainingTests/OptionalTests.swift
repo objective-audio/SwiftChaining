64fb9d3e356dca41b44e0825b227cbcef940309e
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-07 17:52:01 +0900

    add tests

--- SwiftChainingTests/OptionalTests.swift
+++ SwiftChainingTests/OptionalTests.swift
@@ -28,4 +28,43 @@ class OptionalTests: XCTestCase {
         
         observer.invalidate()
     }
+    
+    func testGuardUnwrap() {
+        let notifier = Notifier<Int?>()
+        
+        var received: [Int] = []
+        
+        let observer = notifier.chain().guardUnwrap().do { received.append($0) }.end()
+        
+        notifier.notify(value: 1)
+        
+        XCTAssertEqual(received.count, 1)
+        XCTAssertEqual(received[0], 1)
+        
+        XCTAssertNoThrow(notifier.notify(value: nil))
+        
+        XCTAssertEqual(received.count, 1)
+        
+        observer.invalidate()
+    }
+    
+    func testForceUnwrap() {
+        let notifier = Notifier<Int?>()
+        
+        var received: [Int] = []
+        
+        let observer = notifier.chain().forceUnwrap().do { received.append($0) }.end()
+        
+        notifier.notify(value: 1)
+        
+        XCTAssertEqual(received.count, 1)
+        XCTAssertEqual(received[0], 1)
+        
+        // fatal error
+        // notifier.notify(value: nil)
+        
+        XCTAssertEqual(received.count, 1)
+        
+        observer.invalidate()
+    }
 }
