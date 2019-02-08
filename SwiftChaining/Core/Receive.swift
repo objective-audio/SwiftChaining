35995ec09b9526e2c032da5bfa75fd0ed331e139
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-09 00:15:50 +0900

    receive with keypath

--- SwiftChaining/Core/Receive.swift
+++ SwiftChaining/Core/Receive.swift
@@ -11,9 +11,9 @@ extension Chain {
         }
     }
     
-    public func receive<R: Receivable>(_ receiver: R) -> Chain<HandlerOut, HandlerIn, Sender> where R.ReceiveValue == Void {
-        return self.do { [weak receiver] _ in
-            receiver?.receive(value: ())
+    public func receive<Root: AnyObject>(_ receiver: Root, keyPath: ReferenceWritableKeyPath<Root, HandlerOut>) -> Chain<HandlerOut, HandlerIn, Sender> {
+        return self.do { [weak receiver] value in
+            receiver?[keyPath: keyPath] = value
         }
     }
 }
