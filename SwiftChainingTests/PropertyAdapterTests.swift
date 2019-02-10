1a1adc903155b36ee630822e18878ac7625ef1ce
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-10 11:51:34 +0900

    PropertyAdapter

---
+++ SwiftChainingTests/PropertyAdapterTests.swift
@@ -0,0 +1,71 @@
+//
+//  PropertyAdapterTests.swift
+//
+
+import XCTest
+import Chaining
+
+class PropertyAdapterTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+    
+    func testValue() {
+        class TestClass {
+            var value: Int = 0
+        }
+        
+        let testObj = TestClass()
+        let adapter = PropertyAdapter(testObj, keyPath: \TestClass.value)
+        
+        XCTAssertEqual(adapter.value, 0)
+        
+        adapter.value = 1
+        
+        XCTAssertEqual(adapter.value, 1)
+        XCTAssertEqual(testObj.value, 1)
+    }
+    
+    func testSafeValue() {
+        class TestClass {
+            var value: Int = 0
+        }
+            
+        var testObj: TestClass? = TestClass()
+        
+        let adapter = PropertyAdapter(testObj!, keyPath: \TestClass.value)
+        
+        XCTAssertNotNil(adapter.safeValue)
+        XCTAssertEqual(adapter.safeValue, 0)
+        
+        testObj = nil
+        
+        XCTAssertNil(adapter.safeValue)
+    }
+
+    func testChain() {
+        class TestClass {
+            var value: Int = 0
+        }
+        
+        let testObj = TestClass()
+        let adapter = PropertyAdapter(testObj, keyPath: \TestClass.value)
+        
+        var received: [Int] = []
+        
+        let observer = adapter.chain().do { received.append($0) }.sync()
+        
+        XCTAssertEqual(received.count, 1)
+        XCTAssertEqual(received[0], 0)
+        
+        adapter.value = 1
+        
+        XCTAssertEqual(received.count, 2)
+        XCTAssertEqual(received[1], 1)
+        
+        observer.invalidate()
+    }
+}
