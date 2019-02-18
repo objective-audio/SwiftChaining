974a222865d5f2abb1ed40e33664ffe28b13ffbf
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-18 10:14:00 +0900

    refactoring tuple

--- SwiftChaining/Core/Tuple.swift
+++ SwiftChaining/Core/Tuple.swift
@@ -5,62 +5,68 @@
 import Foundation
 
 extension Chain where Sender: Fetchable {
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), Sender> {
-        return _tupleToMain(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender> {
+        return _tuple0(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), Sender> where SS1: Fetchable {
-        return _tupleToMain(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender> where Sender1: Fetchable {
+        return _tuple0(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), Sender> where SS2: Fetchable {
-        return _tupleToMain(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender> where Sender2: Fetchable {
+        return _tuple0(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), Sender> where SS1: Fetchable, SS2: Fetchable {
-        return _tupleToMain(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender> where Sender1: Fetchable, Sender2: Fetchable {
+        return _tuple0(self, chain1, chain2)
     }
 }
 
 extension Chain {
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), Sender> {
-        return _tupleToMain(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender> {
+        return _tuple0(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), SS1> where SS1: Fetchable {
-        return _tupleToSub1(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender1> where Sender1: Fetchable {
+        return _tuple1(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), SS2> where SS2: Fetchable {
-        return _tupleToSub2(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender2> where Sender2: Fetchable {
+        return _tuple2(self, chain1, chain2)
     }
     
-    public func tuple<SO1, SI1, SS1, SO2, SI2, SS2>(_ subChain1: Chain<SO1, SI1, SS1>,
-                                                    _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(Out?, SO1?, SO2?), ((Out?, SO1?)?, SO2?), SS1> where SS1: Fetchable, SS2: Fetchable {
-        return _tupleToSub1(self, subChain1, subChain2)
+    public func tuple<Out1, In1, Sender1, Out2, In2, Sender2>(_ chain1: Chain<Out1, In1, Sender1>,
+                                                              _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out?, Out1?, Out2?), ((Out?, Out1?)?, Out2?), Sender1> where Sender1: Fetchable, Sender2: Fetchable {
+        return _tuple1(self, chain1, chain2)
     }
 }
 
-private func _tupleToMain<MO, MI, MS, SO1, SI1, SS1, SO2, SI2, SS2>(_ mainChain: Chain<MO, MI, MS>, _ subChain1: Chain<SO1, SI1, SS1>, _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(MO?, SO1?, SO2?), ((MO?, SO1?)?, SO2?), MS> {
-    return mainChain.tuple(subChain1).tuple(subChain2).map(_flat)
+private func _tuple0<Out0, In0, Sender0, Out1, In1, Sender1, Out2, In2, Sender2>(_ chain0: Chain<Out0, In0, Sender0>,
+                                                                                 _ chain1: Chain<Out1, In1, Sender1>,
+                                                                                 _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out0?, Out1?, Out2?), ((Out0?, Out1?)?, Out2?), Sender0> {
+    return chain0.tuple(chain1).tuple(chain2).map(_flat3)
 }
 
-private func _tupleToSub1<MO, MI, MS, SO1, SI1, SS1, SO2, SI2, SS2>(_ mainChain: Chain<MO, MI, MS>, _ subChain1: Chain<SO1, SI1, SS1>, _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(MO?, SO1?, SO2?), ((MO?, SO1?)?, SO2?), SS1> {
-    return _tupleToSub(main: mainChain, sub: subChain1).tuple(subChain2).map(_flat)
+private func _tuple1<Out0, In0, Sender0, Out1, In1, Sender1, Out2, In2, Sender2>(_ chain0: Chain<Out0, In0, Sender0>,
+                                                                                 _ chain1: Chain<Out1, In1, Sender1>,
+                                                                                 _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out0?, Out1?, Out2?), ((Out0?, Out1?)?, Out2?), Sender1> {
+    return _tuple1(chain0: chain0, chain1: chain1).tuple(chain2).map(_flat3)
 }
 
-private func _tupleToSub2<MO, MI, MS, SO1, SI1, SS1, SO2, SI2, SS2>(_ mainChain: Chain<MO, MI, MS>, _ subChain1: Chain<SO1, SI1, SS1>, _ subChain2: Chain<SO2, SI2, SS2>) -> Chain<(MO?, SO1?, SO2?), ((MO?, SO1?)?, SO2?), SS2> {
-    return _tupleToSub(main: mainChain.tuple(subChain1), sub: subChain2).map(_flat)
+private func _tuple2<Out0, In0, Sender0, Out1, In1, Sender1, Out2, In2, Sender2>(_ chain0: Chain<Out0, In0, Sender0>,
+                                                                                 _ chain1: Chain<Out1, In1, Sender1>,
+                                                                                 _ chain2: Chain<Out2, In2, Sender2>) -> Chain<(Out0?, Out1?, Out2?), ((Out0?, Out1?)?, Out2?), Sender2> {
+    return _tuple1(chain0: chain0.tuple(chain1), chain1: chain2).map(_flat3)
 }
 
-private func _flat<MO, SO1, SO2>(_ lhs: (MO?, SO1?)?, _ rhs: SO2?) -> (MO?, SO1?, SO2?) {
+private func _flat3<Out0, Out1, Out2>(_ lhs: (Out0?, Out1?)?, _ rhs: Out2?) -> (Out0?, Out1?, Out2?) {
     if let lhs = lhs {
         return (lhs.0, lhs.1, nil)
     } else if let rhs = rhs {
@@ -69,3 +75,6 @@ private func _flat<MO, SO1, SO2>(_ lhs: (MO?, SO1?)?, _ rhs: SO2?) -> (MO?, SO1?
         fatalError()
     }
 }
+
+
+
