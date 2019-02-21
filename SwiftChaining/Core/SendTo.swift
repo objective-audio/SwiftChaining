8ffbd8614398b1a3748d85aa1f3ae9e67734382e
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-21 23:08:57 +0900

    sendXTo

--- SwiftChaining/Core/SendTo.swift
+++ SwiftChaining/Core/SendTo.swift
@@ -16,4 +16,34 @@ extension Chain {
             receiver?[keyPath: keyPath] = value
         }
     }
+    
+    public func send0To<R: Receivable, T0, T1>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1), R.ReceiveValue == T0 {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value.0)
+        }
+    }
+    
+    public func send0To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2), R.ReceiveValue == T0 {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value.0)
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value.1)
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value.1)
+        }
+    }
+    
+    public func send2To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2), R.ReceiveValue == T2 {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value.2)
+        }
+    }
 }
