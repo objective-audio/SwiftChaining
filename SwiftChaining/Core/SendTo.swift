7be8527471bc5fc3fd98a4b98c65df79136bae77
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-22 22:14:13 +0900

    send optional

--- SwiftChaining/Core/SendTo.swift
+++ SwiftChaining/Core/SendTo.swift
@@ -11,6 +11,12 @@ extension Chain {
         }
     }
     
+    public func sendTo<R: Receivable>(_ receiver: R) -> Chain<Out, In, Sender> where R.ReceiveValue == Out? {
+        return self.do { [weak receiver] value in
+            receiver?.receive(value: value)
+        }
+    }
+    
     public func sendTo<Root: AnyObject>(_ receiver: Root, keyPath: ReferenceWritableKeyPath<Root, Out>) -> Chain<Out, In, Sender> {
         return self.do { [weak receiver] value in
             receiver?[keyPath: keyPath] = value
