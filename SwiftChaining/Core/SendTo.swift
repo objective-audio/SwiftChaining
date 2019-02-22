e883e087028791fbb5b99d75814d8a9062285427
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-22 21:50:38 +0900

    send optional to

--- SwiftChaining/Core/SendTo.swift
+++ SwiftChaining/Core/SendTo.swift
@@ -41,12 +41,6 @@ extension Chain {
         }
     }
     
-    public func send0To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T0 {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value.0)
-        }
-    }
-    
     public func send1To<R: Receivable, T0, T1>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1), R.ReceiveValue == T1 {
         return self.do { [weak receiver] value in
             receiver?.receive(value: value.1)
@@ -71,12 +65,6 @@ extension Chain {
         }
     }
     
-    public func send1To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T1 {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value.1)
-        }
-    }
-    
     public func send2To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2), R.ReceiveValue == T2 {
         return self.do { [weak receiver] value in
             receiver?.receive(value: value.2)
@@ -95,12 +83,6 @@ extension Chain {
         }
     }
     
-    public func send2To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T2 {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value.2)
-        }
-    }
-    
     public func send3To<R: Receivable, T0, T1, T2, T3>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3), R.ReceiveValue == T3 {
         return self.do { [weak receiver] value in
             receiver?.receive(value: value.3)
@@ -113,27 +95,129 @@ extension Chain {
         }
     }
     
-    public func send3To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T3 {
-        return self.do { [weak receiver] value in
-            receiver?.receive(value: value.3)
-        }
-    }
-    
     public func send4To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4), R.ReceiveValue == T4 {
         return self.do { [weak receiver] value in
             receiver?.receive(value: value.4)
         }
     }
     
-    public func send4To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T4 {
+    public func sendTo<R: Receivable, T>(_ receiver: R) -> Chain<Out, In, Sender> where Out == T?, R.ReceiveValue == T {
         return self.do { [weak receiver] value in
-            receiver?.receive(value: value.4)
+            if let value = value {
+                receiver?.receive(value: value)
+            }
         }
     }
     
-    public func send5To<R: Receivable, T0, T1, T2, T3, T4, T5>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4, T5), R.ReceiveValue == T5 {
+    public func send0To<R: Receivable, T0, T1>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0?, T1), R.ReceiveValue == T0 {
         return self.do { [weak receiver] value in
-            receiver?.receive(value: value.5)
+            if let value = value.0 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send0To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0?, T1, T2), R.ReceiveValue == T0 {
+        return self.do { [weak receiver] value in
+            if let value = value.0 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send0To<R: Receivable, T0, T1, T2, T3>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0?, T1, T2, T3), R.ReceiveValue == T0 {
+        return self.do { [weak receiver] value in
+            if let value = value.0 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send0To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0?, T1, T2, T3, T4), R.ReceiveValue == T0 {
+        return self.do { [weak receiver] value in
+            if let value = value.0 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1?), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            if let value = value.1 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1?, T2), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            if let value = value.1 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1, T2, T3>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1?, T2, T3), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            if let value = value.1 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send1To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1?, T2, T3, T4), R.ReceiveValue == T1 {
+        return self.do { [weak receiver] value in
+            if let value = value.1 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send2To<R: Receivable, T0, T1, T2>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2?), R.ReceiveValue == T2 {
+        return self.do { [weak receiver] value in
+            if let value = value.2 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send2To<R: Receivable, T0, T1, T2, T3>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2?, T3), R.ReceiveValue == T2 {
+        return self.do { [weak receiver] value in
+            if let value = value.2 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send2To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2?, T3, T4), R.ReceiveValue == T2 {
+        return self.do { [weak receiver] value in
+            if let value = value.2 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send3To<R: Receivable, T0, T1, T2, T3>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3?), R.ReceiveValue == T3 {
+        return self.do { [weak receiver] value in
+            if let value = value.3 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send3To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3?, T4), R.ReceiveValue == T3 {
+        return self.do { [weak receiver] value in
+            if let value = value.3 {
+                receiver?.receive(value: value)
+            }
+        }
+    }
+    
+    public func send4To<R: Receivable, T0, T1, T2, T3, T4>(_ receiver: R) -> Chain<Out, In, Sender> where Out == (T0, T1, T2, T3, T4?), R.ReceiveValue == T4 {
+        return self.do { [weak receiver] value in
+            if let value = value.4 {
+                receiver?.receive(value: value)
+            }
         }
     }
 }
--- SwiftChainingTests/SendToTests.swift
+++ SwiftChainingTests/SendToTests.swift
@@ -132,33 +132,116 @@ class SendToTests: XCTestCase {
         observer.invalidate()
     }
     
-    func testSendXTo_tuple6() {
+    func testSendOptionalTo() {
+        let sender = ValueHolder<Int?>(0)
+        let receiver = ValueHolder(10)
+        
+        let observer = sender.chain().sendTo(receiver).sync()
+        
+        XCTAssertEqual(receiver.value, 0)
+        
+        sender.value = nil
+        
+        XCTAssertEqual(receiver.value, 0)
+        
+        observer.invalidate()
+    }
+    
+    func testSendOptionalXTo_tuple2() {
+        let sender0 = ValueHolder(0)
+        let sender1 = ValueHolder("1")
+        
+        let receiver0 = ValueHolder(10)
+        let receiver1 = ValueHolder("11")
+        
+        let observer = sender0.chain()
+            .tuple(sender1.chain())
+            .send0To(receiver0)
+            .send1To(receiver1)
+            .sync()
+        
+        XCTAssertEqual(receiver0.value, 0)
+        XCTAssertEqual(receiver1.value, "1")
+        
+        observer.invalidate()
+    }
+    
+    func testSendOptionalXTo_tuple3() {
+        let sender0 = ValueHolder(0)
+        let sender1 = ValueHolder("1")
+        let sender2 = ValueHolder(2.0)
+        
+        let receiver0 = ValueHolder(10)
+        let receiver1 = ValueHolder("11")
+        let receiver2 = ValueHolder(12.0)
+        
+        let observer = sender0.chain()
+            .tuple(sender1.chain())
+            .tuple(sender2.chain())
+            .send0To(receiver0)
+            .send1To(receiver1)
+            .send2To(receiver2)
+            .sync()
+        
+        XCTAssertEqual(receiver0.value, 0)
+        XCTAssertEqual(receiver1.value, "1")
+        XCTAssertEqual(receiver2.value, 2.0)
+        
+        observer.invalidate()
+    }
+    
+    func testSendOptionalXTo_tuple4() {
+        let sender0 = ValueHolder(0)
+        let sender1 = ValueHolder("1")
+        let sender2 = ValueHolder(2.0)
+        let sender3 = ValueHolder(3)
+        
+        let receiver0 = ValueHolder(10)
+        let receiver1 = ValueHolder("11")
+        let receiver2 = ValueHolder(12.0)
+        let receiver3 = ValueHolder(13)
+        
+        let observer = sender0.chain()
+            .tuple(sender1.chain())
+            .tuple(sender2.chain())
+            .tuple(sender3.chain())
+            .send0To(receiver0)
+            .send1To(receiver1)
+            .send2To(receiver2)
+            .send3To(receiver3)
+            .sync()
+        
+        XCTAssertEqual(receiver0.value, 0)
+        XCTAssertEqual(receiver1.value, "1")
+        XCTAssertEqual(receiver2.value, 2.0)
+        XCTAssertEqual(receiver3.value, 3)
+        
+        observer.invalidate()
+    }
+    
+    func testSendOptionalXTo_tuple5() {
         let sender0 = ValueHolder(0)
         let sender1 = ValueHolder("1")
         let sender2 = ValueHolder(2.0)
         let sender3 = ValueHolder(3)
         let sender4 = ValueHolder("4")
-        let sender5 = ValueHolder(5.0)
         
         let receiver0 = ValueHolder(10)
         let receiver1 = ValueHolder("11")
         let receiver2 = ValueHolder(12.0)
         let receiver3 = ValueHolder(13)
         let receiver4 = ValueHolder("14")
-        let receiver5 = ValueHolder(15.0)
         
         let observer = sender0.chain()
-            .combine(sender1.chain())
-            .combine(sender2.chain())
-            .combine(sender3.chain())
-            .combine(sender4.chain())
-            .combine(sender5.chain())
+            .tuple(sender1.chain())
+            .tuple(sender2.chain())
+            .tuple(sender3.chain())
+            .tuple(sender4.chain())
             .send0To(receiver0)
             .send1To(receiver1)
             .send2To(receiver2)
             .send3To(receiver3)
             .send4To(receiver4)
-            .send5To(receiver5)
             .sync()
         
         XCTAssertEqual(receiver0.value, 0)
@@ -166,7 +249,6 @@ class SendToTests: XCTestCase {
         XCTAssertEqual(receiver2.value, 2.0)
         XCTAssertEqual(receiver3.value, 3)
         XCTAssertEqual(receiver4.value, "4")
-        XCTAssertEqual(receiver5.value, 5.0)
         
         observer.invalidate()
     }
