b92ed1b4e2e74ee20f337f0a962e8bc71cef5380
Yuki Yasoshima <yukiyasos@gmail.com>
2019-02-27 21:16:58 +0900

    add tests

--- SwiftChainingTests/KVOAdapterTests.swift
+++ SwiftChainingTests/KVOAdapterTests.swift
@@ -18,7 +18,7 @@ class KVOAdapterTests: XCTestCase {
         super.tearDown()
     }
 
-    func testKVOAdapter() {
+    func testSetByOriginal() {
         let object = TestObject()
         
         let adapter = KVOAdapter(object, keyPath: \TestObject.text)
@@ -36,6 +36,41 @@ class KVOAdapterTests: XCTestCase {
         XCTAssertEqual(received[1], "test_value")
         
         observer.invalidate()
+    }
+    
+    func testSetByAdapter() {
+        let object = TestObject()
+        
+        let adapter = KVOAdapter(object, keyPath: \TestObject.text)
+        
+        var received: [String] = []
+        
+        let observer = adapter.chain().do { received.append($0) }.sync()
+        
+        XCTAssertEqual(received.count, 1)
+        XCTAssertEqual(received[0], "initial")
+        
+        adapter.value = "test_value"
+        
+        XCTAssertEqual(received.count, 2)
+        XCTAssertEqual(received[1], "test_value")
+        
+        observer.invalidate()
+    }
+    
+    func testSafeValue() {
+        var adapter: KVOAdapter<TestObject, String>?
+        
+        do {
+            let object = TestObject()
+            
+            adapter = KVOAdapter(object, keyPath: \TestObject.text)
+            
+            XCTAssertEqual(adapter?.safeValue, "initial")
+        }
+        
+        XCTAssertNotNil(adapter)
+        XCTAssertNil(adapter?.safeValue)
     }
     
     func testInvalidate() {
@@ -83,5 +118,20 @@ class KVOAdapterTests: XCTestCase {
         object.text = "test_value_2"
         
         XCTAssertEqual(received.count, 2)
+    }
+    
+    func testRecursive() {
+        let object = TestObject()
+        let adapter = KVOAdapter(object, keyPath: \TestObject.text)
+        
+        var received: [String] = []
+        
+        let observer = adapter.chain().do { received.append($0) }.sendTo(adapter).end()
+        
+        adapter.value = "test_value"
+        
+        XCTAssertEqual(received.count, 1)
+        
+        observer.invalidate()
     }
 }
