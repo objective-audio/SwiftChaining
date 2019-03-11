32a5b4b4e54330488d96f4ab649a161982a380c1
Yuki Yasoshima <yukiyasos@gmail.com>
2019-03-11 22:03:45 +0900

    add EventableValueHolder

---
+++ SwiftChaining/Additions/EventableValueHolder.swift
@@ -0,0 +1,71 @@
+//
+//  EventableValueHolder.swift
+//
+
+import Foundation
+
+final public class EventableValueHolder<T> {
+    public enum Event {
+        case fetched(T)
+        case current(T)
+    }
+    
+    public private(set) var rawValue: T
+    
+    public init(_ initial: T) {
+        self.rawValue = initial
+    }
+    
+    public var value: T {
+        set {
+            self.rawValue = newValue
+            self.broadcast(value: .current(newValue))
+        }
+        get { return self.rawValue }
+    }
+}
+
+extension EventableValueHolder: Fetchable {
+    public typealias SendValue = Event
+    
+    public func fetchedValue() -> Event {
+        return .fetched(self.value)
+    }
+}
+
+extension EventableValueHolder where T: Equatable {
+    public var value: T {
+        set {
+            if self.rawValue != newValue {
+                self.rawValue = newValue
+                self.broadcast(value: .current(newValue))
+            }
+        }
+        get { return self.rawValue }
+    }
+}
+
+extension EventableValueHolder: Receivable {
+    public func receive(value: T) {
+        self.value = value
+    }
+}
+
+extension EventableValueHolder: Equatable where T: Equatable {
+    public static func == (lhs: EventableValueHolder, rhs: EventableValueHolder) -> Bool {
+        return lhs.rawValue == rhs.rawValue
+    }
+}
+
+extension EventableValueHolder.Event: Equatable where T: Equatable {
+    public static func == (lhs: EventableValueHolder.Event, rhs: EventableValueHolder.Event) -> Bool {
+        switch (lhs, rhs) {
+        case (.fetched(let lhsFetched), .fetched(let rhsFetched)):
+            return lhsFetched == rhsFetched
+        case (.current(let lhsCurrent), .current(let rhsCurrent)):
+            return lhsCurrent == rhsCurrent
+        default:
+            return false
+        }
+    }
+}
