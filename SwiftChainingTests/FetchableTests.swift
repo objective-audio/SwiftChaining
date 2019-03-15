234a13b5e78021f8ad23a60d10109b09101595f5
Yuki Yasoshima <yukiyasos@gmail.com>
2019-03-15 10:11:46 +0900

    separate fetcher tests

--- SwiftChainingTests/FetchableTests.swift
+++ SwiftChainingTests/FetchableTests.swift
@@ -24,54 +24,6 @@ class FetchableTests: XCTestCase {
         super.tearDown()
     }
 
-    func testFetcher() {
-        let fetcher = Fetcher<Int> { 1 }
-        
-        var received: [Int] = []
-        
-        let observer = fetcher.chain().do { received.append($0) }.sync()
-        
-        // syncでフェッチされる
-        XCTAssertEqual(received.count, 1)
-        XCTAssertEqual(received[0], 1)
-        
-        observer.invalidate()
-    }
-    
-    func testFetcherReceivable() {
-        let fetcher = Fetcher<Int> { 1 }
-        
-        var received: [Int] = []
-        
-        let observer = fetcher.chain().do { received.append($0) }.end()
-        
-        XCTAssertEqual(received.count, 0)
-        
-        fetcher.receive(value: ())
-        
-        XCTAssertEqual(received.count, 1)
-        XCTAssertEqual(received[0], 1)
-        
-        observer.invalidate()
-    }
-    
-    func testFetcherBroadcast() {
-        let fetcher = Fetcher<Int> { 1 }
-        
-        var received: [Int] = []
-        
-        let observer = fetcher.chain().do { received.append($0) }.end()
-        
-        XCTAssertEqual(received.count, 0)
-        
-        fetcher.broadcast()
-        
-        XCTAssertEqual(received.count, 1)
-        XCTAssertEqual(received[0], 1)
-        
-        observer.invalidate()
-    }
-    
     func testFetchOnlyJustSynced() {
         let pool = ObserverPool()
         
