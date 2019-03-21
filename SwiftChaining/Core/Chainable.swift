e165815e697ff7cd2af7d251af38a97886ff9e2a
Yuki Yasoshima <yukiyasos@gmail.com>
2019-03-21 10:12:21 +0900

    move functions Sendable to Chainable

--- SwiftChaining/Core/Chainable.swift
+++ SwiftChaining/Core/Chainable.swift
@@ -12,4 +12,31 @@ public protocol Chainable: class {
 }
 
 extension Chainable {
+    public func fetch(for: JointClass) {}
+    
+    public func retain() -> Retainer<Self> {
+        return Retainer(object: self)
+    }
+    
+    public func chain() -> SenderChain {
+        return self.chain(retained: false)
+    }
+    
+    internal func chain(retained: Bool) -> SenderChain {
+        let core = CoreGlobal.getOrCreateCore(for: self)
+        
+        let sender: Reference<Self> = retained ? .strong(self) : .weak(Weak(self))
+        
+        let joint = core.addJoint(sender: sender)
+        
+        let handler0: JointHandler<SendValue> = { value, joint in
+            if let nextHandler = joint.handler(at: 1) as? JointHandler<SendValue> {
+                nextHandler(value, joint)
+            }
+        }
+        
+        joint.appendHandler(handler0)
+        
+        return SenderChain(joint: joint)
+    }
 }
--- SwiftChaining/Core/Sendable.swift
+++ SwiftChaining/Core/Sendable.swift
@@ -8,35 +8,7 @@ public protocol Sendable: Chainable {
 }
 
 extension Sendable {
-    public func fetch(for: JointClass) {}
-    
     public func broadcast(value: SendValue) {
         CoreGlobal.core(for: self)?.broadcast(value: value)
-    }
-    
-    public func retain() -> Retainer<Self> {
-        return Retainer(object: self)
-    }
-    
-    public func chain() -> SenderChain {
-        return self.chain(retained: false)
-    }
-    
-    internal func chain(retained: Bool) -> SenderChain {
-        let core = CoreGlobal.getOrCreateCore(for: self)
-        
-        let sender: Reference<Self> = retained ? .strong(self) : .weak(Weak(self))
-        
-        let joint = core.addJoint(sender: sender)
-        
-        let handler0: JointHandler<SendValue> = { value, joint in
-            if let nextHandler = joint.handler(at: 1) as? JointHandler<SendValue> {
-                nextHandler(value, joint)
-            }
-        }
-        
-        joint.appendHandler(handler0)
-        
-        return SenderChain(joint: joint)
     }
 }
